35b5e7323ba0486e2b64117d67be41a3a343220d
Rohit <rohitkrrt@gmail.com>
2025-03-20 19:17:17 +0530

    Feat: Implemented support for custom delimiters

--- src/calculator.ts
+++ src/calculator.ts
@@ -2,7 +2,15 @@
 
 export function add(numbers: string): number {
     if (numbers === "") return 0;
+  
+    let delimiter = /,|\n/;
     
-    const numArray = numbers.replace(/\n/g, ",").split(",").map(Number);
+    if (numbers.startsWith("//")) {
+      const parts = numbers.split("\n");
+      delimiter = new RegExp(parts[0].substring(2)); // Extract delimiter
+      numbers = parts[1]; // Remaining numbers
+    }
+  
+    const numArray = numbers.split(delimiter).map(Number);
     return numArray.reduce((sum, num) => sum + num, 0);
   }
