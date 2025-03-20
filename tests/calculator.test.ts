6afa6f6ddbdc2bd97848e2389224fbf4770e9c97
Rohit <rohitkrrt@gmail.com>
2025-03-20 19:10:29 +0530

    Test: Add function should return sum of two numbers

--- tests/calculator.test.ts
+++ tests/calculator.test.ts
@@ -7,3 +7,7 @@ test("returns 0 for an empty string", () => {
 test("returns the number itself when given one number", () => {
     expect(add("1")).toBe(1);
   });
+
+  test("returns sum of two numbers", () => {
+    expect(add("1,5")).toBe(6);
+  });
