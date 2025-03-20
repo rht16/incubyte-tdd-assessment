07e37fb5f5b0d14b509d17e0bbfe4aa2fd7e8a10
Rohit <rohitkrrt@gmail.com>
2025-03-20 19:13:05 +0530

    Feat: Code refatored  Implemented support for newline delimiters

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,9 +1,4 @@
-// export function add(numbers: string): number {
-//     if (numbers === "") return 0;
-    
-//     const numArray = numbers.split(",").map(Number);
-//     return numArray.reduce((sum, num) => sum + num, 0);
-//   }
+
 
 export function add(numbers: string): number {
     if (numbers === "") return 0;
