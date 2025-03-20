7c0e0c53a18f9c4c77ec322019155371367adc9c
Rohit <rohitkrrt@gmail.com>
2025-03-20 19:09:24 +0530

    Feat: Updated add function to return single number

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,4 +1,5 @@
 export function add(numbers: string): number {
     if (numbers === "") return 0;
+    if (!numbers.includes(",")) return parseInt(numbers, 10);
     return 0; // Temporary return value
   }
